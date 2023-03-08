58c1dcccb21a87dc3d041e319c6da76d122edc37
singhj-git <121680198+singhj-git@users.noreply.github.com>
2023-03-08 13:23:34 -0500

    Add files via upload

---
+++ LWCSample/force-app/main/default/lwc/firstLWC/firstLWC.js
@@ -0,0 +1,9 @@
+import{ LightningElement } from 'lwc';
+
+export default class FirstLWC extends LightningElement {
+    name='Vijay';
+    studentId='536934';
+    course='computer science';
+    floor='third floor'
+    contact='2345342121'
+}
